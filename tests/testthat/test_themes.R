9291df4f53a0d694f0b793740fe2ad1dd00822f3
Jonathan Gilligan <jonathan.gilligan@vanderbilt.edu>
2025-11-25 16:12:05 -0600

    Updated testthat test_themes.R and R-CMD-check GitHub action.

--- tests/testthat/test_themes.R
+++ tests/testthat/test_themes.R
@@ -13,24 +13,28 @@ test_theme <- function(theme) {
     skip_on_cran()
 
     # work in a temp directory
-    dir <- tempfile()
-    dir.create(dir)
-    oldwd <- setwd(dir)
+    tmpdir <- tempdir(check = TRUE)
+    tstdir <- tempfile("revealjg-test", tmpdir)
+    dir.create(tstdir)
+    oldwd <- setwd(tstdir)
     on.exit(setwd(oldwd), add = TRUE)
+    message("Set directory to temporary directory ", tstdir)
 
     # create a draft of a presentation
     testdoc <- "testdoc.Rmd"
-    rmarkdown::draft(testdoc,
-                     system.file("rmarkdown", "templates", "revealjs_presentation",
-                                 package = "revealjg"),
-                     create_dir = FALSE,
-                     edit = FALSE)
+    rmd_file <- rmarkdown::draft(
+      testdoc,
+      system.file("rmarkdown", "templates", "revealjs_presentation",
+                  package = "revealjg"),
+      create_dir = FALSE,
+      edit = FALSE
+      )
 
     # render it with the specified theme
     capture.output({
-      output_file <- tempfile(fileext = ".html")
+      output_file <- tempfile(fileext = ".html", tstdir)
       output_format <- revealjs_presentation(theme = theme)
-      rmarkdown::render(testdoc,
+      rmarkdown::render(rmd_file,
                         output_format = output_format,
                         output_file = output_file)
       expect_true(file.exists(output_file))
